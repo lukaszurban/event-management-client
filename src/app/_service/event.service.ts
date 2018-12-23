211b3581bfd4146d2d9e03c7ba4f41f2e6e4ec20
lukaszurban <urbanlukasz94@gmail.com>
2018-12-23 16:05:28 +0100

    added event's model and service

---
+++ src/app/_service/event.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Event} from '../_model/event';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  readonly apiUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getEvents(): Observable<Event[]> {
+    const url = this.apiUrl + '/events';
+    return this.http.get<Event[]>(url);
+  }
+
+}
